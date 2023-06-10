34c3e5b96d4c4a84e72120a41dfa9e01888dd6f1
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-06-09 23:25:58 -0700

    updated textures

--- player.py
+++ player.py
@@ -7,9 +7,9 @@ class Player(pygame.sprite.Sprite):
         super().__init__(group)
         self.import_assets()
         self.frame_index = 0
-        self.image = self.idle_frames[self.frame_index]
+        self.status = "idle"
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center=pos)
-        self.status = "down"
         self.z = 7
 
         #movement
@@ -20,16 +20,14 @@ class Player(pygame.sprite.Sprite):
     def import_assets(self):
         self.file = SpriteSheet("sprites.png")
 
-        self.walk_frames = [
-            self.file.parse_sprite("player3.png"),
-            self.file.parse_sprite("player4.png"),
-            self.file.parse_sprite("player5.png"),
-        ]
-       
-        self.idle_frames = [
-            self.file.parse_sprite("player1.png"),
-            self.file.parse_sprite("player2.png")
-        ]
+        self.animations = {
+            'idle':[self.file.parse_sprite("player1.png"),self.file.parse_sprite("player2.png")],
+            'right':[self.file.parse_sprite("player3.png"),self.file.parse_sprite("player4.png"),self.file.parse_sprite("player5.png")],
+            'left':[pygame.transform.flip(self.file.parse_sprite("player3.png"),True,False),pygame.transform.flip(self.file.parse_sprite("player4.png"),True,False),pygame.transform.flip(self.file.parse_sprite("player5.png"),True,False)],
+            'up':[self.file.parse_sprite("player8.png"),self.file.parse_sprite("player9.png"),self.file.parse_sprite("player10.png")],
+            'down':[self.file.parse_sprite("player11.png"),self.file.parse_sprite("player12.png")],
+            'up_idle':[self.file.parse_sprite("player6.png"),self.file.parse_sprite("player7.png")],
+        }
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -51,13 +49,19 @@ class Player(pygame.sprite.Sprite):
             self.status = "left"
         else:
             self.direction.x = 0
+     
+           
 
     def animate(self,dt):
-        self.frame_index += 2 * dt
-        if self.frame_index >=len(self.idle_frames):
+        if self.status == "idle":
+            self.frame_index += 4 * dt
+        else:
+            self.frame_index += 6 * dt
+        if self.frame_index >=len(self.animations[self.status]):
             self.frame_index = 0
+            print(self.status)
 
-        self.image = self.idle_frames[int(self.frame_index)]
+        self.image = self.animations[self.status][int(self.frame_index)]
     
     def move(self,dt):
         if self.direction.magnitude() > 0:
